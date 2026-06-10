9f5b26bc7b62508131f76910c511b8e64af12135
Aris181106 <nvt181106@gmail.com>
2026-06-10 08:30:22 +0700

    [FEATURE] Create mock data for React

---
+++ html/data/data.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    "id": 1,
+    "Tên sản phẩm": "Samsung S26",
+    "Danh mục": "Điện thoại",
+    "Giá": "15.000.000 đ",
+    "Trạng thái" : "Còn hàng"
+  },
+    {
+    "id": 2,
+    "Tên sản phẩm": "Iphone 17",
+    "Danh mục": "Điện thoại",
+    "Giá": "36.000.000 đ",
+    "Trạng thái" : "Còn hàng"
+  },  {
+    "id": 3,
+    "Tên sản phẩm": "TV LG 50inch",
+    "Danh mục": "TV",
+    "Giá": "50.000.000 đ",
+    "Trạng thái" : "Hết hàng"
+  },  {
+    "id": 4,
+    "Tên sản phẩm": "ASUS TUF",
+    "Danh mục": "Laptop",
+    "Giá": "19.000.000 đ",
+    "Trạng thái" : "Còn hàng"
+  },  {
+    "id": 5,
+    "Tên sản phẩm": "Màn hình BenQ 360hz",
+    "Danh mục": "Phụ kiện",
+    "Giá": "12.000.000 đ",
+    "Trạng thái" : "Còn hàng"
+  },
+]
